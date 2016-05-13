bef4605e3271feb039bbb0f2773fc94b432f503e
andyedwards231 <andyedwards231@gmail.com>
2016-05-13 09:39:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,27 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-              return bot.say('Hi Im Jasper. Just say HELLO to get started.')
-                .then(() => 'speak');
+            return bot.say('Hi! I\'m Jasper!')
+                .then(() => 'askName');
+        }
+    },
+
+    askName: {
+        prompt: (bot) => bot.say('What\'s your name?'),
+        receive: (bot, message) => {
+            const name = message.text;
+            return bot.setProp('name', name)
+                .then(() => bot.say(`Great! I'll call you ${name}`))
+                .then(() => 'finish');
+        }
+    },
+
+    finish: {
+        receive: (bot, message) => {
+            return bot.getProp('name')
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+                        'teach me how to do anything else!'))
+                .then(() => 'finish');
         }    
     },
 
